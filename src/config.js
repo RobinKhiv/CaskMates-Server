f01a2121ddd13336f7184fd3afddf47eb89840a2
Robin Khiv <robinkhiv@gmail.com>
2021-06-22 00:26:36 -0700

    updated config to have database url

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL || 'postgresql://whiskey@localhost/whiskey',
+  DB_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || 'demo-only',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '1h'
 };
